89212ccf0d36fdcc14996784e794fe2a3b493221
Daniel Dang <dondanidang@gmail.com>
2024-01-19 00:30:06 +0100

    Wip

---
+++ app/services/baskets/compute_discount_service.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+module Baskets
+  class ComputeDiscountService < ApplicationService
+    private
+
+    # It initializes a new ComputeDiscountService.
+    #
+    # basket - The Basket to be computed.
+    #
+    def initialize(basket)
+      @basket = basket
+
+      super()
+    end
+
+    # It computes the total discount applicable to the basket.
+    #
+    # Returns total_discount.
+    def call
+      discounts.map do |discount|
+        calculate_discount(discount)
+      end.sum
+    end
+
+    def calculate_discount(discount)
+      affected_product_ids = discount.product_discounts.pluck(:product_id)
+      affected_products = products.select { affected_product_ids.include?(_1.id) }
+
+      discount_rule = discount.rulable
+
+      case discount_rule.operator
+      when 'modulus'
+        handle_modulus(discount_rule, affected_products)
+      when 'gte'
+        handle_gte(discount_rule, affected_products)
+      else
+        raise NotDiscountCalculationImplementedError
+      end
+    end
+
+    def handle_modulus(discount_rule, affected_products)
+      total_product_with_discount = (afffected_products.size / discount_rule.quantity) * discount_rule.quantity
+
+      discount_per_product = case discount_rule.discount_unit
+      when 'percentage'
+        (discount_rule.discount_value / 100.0) * affected_products.first.price
+      when 'cents'
+        Money.new(discount_rule.discount_value)
+      when 'divisor'
+        numerator, denominator = discount_rule.discount_value.split('/').map(&:to_i)
+
+        affected_products.first.price * (numerator / denominator)
+      else
+        raise NotDiscountRuleOperatorImplementedError
+      end
+
+      total_product_with_discount * discount_per_product
+    end
+
+    def handle_gte(discount_rule, affected_products)
+      total_product_with_discount = affected_products.size
+
+      return Money.zero if total_product_with_discount < discount_rule.quantity
+
+      discount_per_product = case discount_rule.discount_unit
+      when 'percentage'
+        (discount_rule.discount_value / 100.0) * affected_products.first.price
+      when 'cents'
+        Money.new(discount_rule.discount_value)
+      when 'divisor'
+        numerator, denominator = discount_rule.discount_value.split('/').map(&:to_i)
+
+        affected_products.first.price * (numerator / denominator)
+      else
+        raise NotDiscountRuleOperatorImplementedError
+      end
+
+      total_product_with_discount * discount_per_product
+    end
+
+    def discounts
+      @discounts ||= Discount
+        .includes(:rulable, :product_discounts)
+        .where(product_discounts: { product_id: product_ids, disabled_at: nil })
+    end
+
+    def products
+      @products ||= @basket.products
+    end
+
+    def product_ids
+      @product_ids ||= products.pluck(:id)
+    end
+  end
+end
--- app/services/baskets/compute_total_service.rb
+++ app/services/baskets/compute_total_service.rb
@@ -19,44 +19,22 @@ module Baskets
     # Returns Basket.
     def call
       @basket.total = total
-      @basket.total_paid = total_with_discount
+      @basket.total_paid = total - total_discount
       @basket.save!
 
       @basket
     end
 
+    def total_discount
+      @total_discount ||= ComputeDiscountService.call(@basket)
+    end
+
     def total
-      Money.new(@basket.products.sum(:price_cents))
-    end
-
-    def total_with_discount
-      products.sum do |product|
-        product_price = product.price
-
-        discounts = discounts_by_product_id[product.id]
-
-        unless discounts.blank?
-          product_price = discounts.map { _1.apply(product_price) }
-        end
-
-        product_price
-      end
-    end
-
-    def discounts
-      @discounts ||= @basket.discounts
-    end
-
-    def discounts_by_product_id
-      @discounts_by_product_id ||= discounts.group_by(&:product_id)
+      @total ||= Money.new(@basket.products.sum(:price_cents))
     end
 
     def products
       @products ||= @basket.products
-    end
-
-    def product_codes
-      @product_codes ||= products.pluck(:code)
     end
   end
 end
