e1a12a8f668c665e14b19619404d3dfdd5effcf3
Daniel Dang <dondanidang@gmail.com>
2024-01-18 22:58:44 +0100

    Add discount logic

--- db/migrations/202401181915_create_product_discounts.rb
+++ db/migrations/202401181915_create_product_discounts.rb
@@ -1,4 +1,4 @@
-class CreateQuantityBasedDiscounts < ActiveRecord::Migration[7.1]
+class CreateProductDiscounts < ActiveRecord::Migration[7.1]
   def change
     create_table :product_discounts do |t|
       t.references :product, null: false, foreign_key: true
--- db/seed.rb
+++ db/seed.rb
@@ -18,7 +18,7 @@ class SeedDataLoader
           product_code: 'GR1',
           discount_value: 50,
           discount_unit: 'percentage',
-          quantity: 2
+          quantity: 2,
           operator: 'modulus'
         },
         {
@@ -27,7 +27,7 @@ class SeedDataLoader
           product_code: 'SR1',
           discount_value: 50,
           discount_unit: 'cents',
-          quantity: 3
+          quantity: 3,
           operator: 'gte'
         },
         {
@@ -36,27 +36,28 @@ class SeedDataLoader
           product_code: 'CF1',
           discount_value: '1/3',
           discount_unit: 'divisor',
-          quantity: 3
+          quantity: 3,
           operator: 'gte'
         }
       ]
 
-      discounts.each do |discount|
-        discount_rule = QuantityBasedDiscount.Create!(
-          discount_value: discount[:discount_value],
-          discount_unit: discount[:discount_unit],
-          quantity: discount[:quantity],
-          operator: discount[:operator]
+      discounts.each do |discount_data|
+        discount_rule = QuantityBasedDiscount.find_or_create_by!(
+          discount_value: discount_data[:discount_value],
+          discount_unit: discount_data[:discount_unit],
+          quantity: discount_data[:quantity],
+          operator: discount_data[:operator]
         )
 
-        discount = Discount.create!(
-          code: discount[:code],
-          description: discount[:description],
+        discount = Discount.find_or_initialize_by(code: discount_data[:code])
+        discount.assign_attributes(
+          description: discount_data[:description],
           rulable: discount_rule
         )
+        discount.save!
 
-        product_discount = ProductDiscount.create!(
-          product: product_by_code[discount[:product_code]],
+        product_discount = ProductDiscount.find_or_create_by!(
+          product: product_by_code[discount_data[:product_code]],
           discount: discount
         )
       end
